40488327efe26a7644796b64e7f08176e83e98d5
nicebits <nicebits@users.noreply.github.com>
2017-03-04 18:59:28 -0800

    Added support for looking up by IP address if DNS fails.

--- crawlcdp.py
+++ crawlcdp.py
@@ -5,6 +5,8 @@ import sys
 from snimpy.manager import (load,Manager)
 import snimpy
 import snimpy.mib
+import pdb
+import socket
 
 snimpy.mib.path("netdisco-mibs/rfc:netdisco-mibs/cisco")
 
@@ -13,21 +15,31 @@ load('CISCO-CDP-MIB')
 
 device_names = {}
 
-def crawl(host):
-	m=snimpy.manager.Manager(host=host, community="public", version=2)
+def crawl(host, ipaddr):
+	try:
+		m=snimpy.manager.Manager(host=host, community="public", version=2)
+	except:
+		print host + " not resolvable, trying IP address " + ipaddr
+		m=snimpy.manager.Manager(host=ipaddr, community="public", version=2)
 
-	device_ids = []
-	for idx in m.cdpCacheDeviceId:
-		device_ids.append(idx)
+	try:
+		device_ids = []
+		for idx in m.cdpCacheDeviceId:
+			device_ids.append(idx)
 
-	for i in device_ids:
-		name = m.cdpCacheDeviceId[i[0],i[1]]
-		if not name in device_names:
-			device_names[name] = 1
-			print name
-			crawl(name)
+		for i in device_ids:
+			name = m.cdpCacheDeviceId[i[0],i[1]]
+			ipaddr = m.cdpCacheAddress[i[0],i[1]]
+			ipaddr = socket.inet_ntoa(ipaddr)
+			#pdb.set_trace()
+			if not name in device_names:
+				device_names[name] = 1
+				print name
+				crawl(name, ipaddr)
+	except:
+		print "Could not communicate with " + host + ", bad credentials?"
 
 if len(sys.argv) != 2:
 	print "Usage: crawlcdp HOST"
 	sys.exit(0)
-crawl(sys.argv[1])
+crawl(sys.argv[1], 0)
